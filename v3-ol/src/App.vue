234f1c32b0b0f5b3e1282432acf3eff66157818c
马志辉 <18489873516@163.com>
2024-07-29 14:26:13 +0800

    Update App.vue

--- v3-ol/src/App.vue
+++ v3-ol/src/App.vue
@@ -29,6 +29,12 @@ onMounted(() => {
 </script>
 
 <template>
+    <div class="card">
+        <button @click="getControls(map)">添加画笔</button>
+        <button @click="addControls(map)">移除画笔</button>
+        <button @click="removeControls(map)">开始编辑</button>
+        <button @click="removeControls(map)">结束编辑</button>
+    </div>
     <div id="map"></div>
 </template>
 
@@ -41,5 +47,19 @@ onMounted(() => {
 #map {
     width: 100vw;
     height: 100vh;
+}
+
+.card {
+    position: absolute;
+    left: 40%;
+    bottom: 5%;
+    z-index: 2;
+}
+
+.card button {
+    margin-left: 1vw;
+    cursor: pointer;
+    padding: 1vh 1vw;
+    border-radius: 1vw;
 }
 </style>
